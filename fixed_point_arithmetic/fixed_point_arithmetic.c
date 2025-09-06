64cfa1bdc830aeafcf961d6140b06dfd68a9af48
Joao Vera <joaoguedessoaresvera@gmail.com>
2025-09-06 11:10:27 +0200

    forgot smt on fixed point

--- fixed_point_arithmetic/fixed_point_arithmetic.c
+++ fixed_point_arithmetic/fixed_point_arithmetic.c
@@ -6,12 +6,6 @@ PROCESS(fixed_point_arithmetic_process, "fixed_point_arithmetic process");
 AUTOSTART_PROCESSES(&fixed_point_arithmetic_process);
 /*---------------------------------------------------------------------------*/
 
-void print_fixed_point(uint16_t x) {
-    uint16_t integer_part = x >> 4;
-    uint16_t fractional_part = x & 0x0F;
-    printf("%u.%u\n", integer_part, fractional_part * 625);
-}
-
 PROCESS_THREAD(fixed_point_arithmetic_process, ev, data) {
     uint16_t a = 0x0025; //2.3 in Q12.4
     uint16_t b = 0x007D; //7.8 in Q12.4
